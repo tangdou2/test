b70393ea91f8d34008d891caa7386c7d4348b31d
wm07091022 <wm07091022@wm07091022-PC>
2020-03-07 15:57:32 +0800

    hh

--- test/src/test/Test1.java
+++ test/src/test/Test1.java
@@ -4,5 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("hello github");
+		System.out.println("hello github");
+		System.out.println("hello github");
 	}
 }
